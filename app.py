19b6be75a81ef19bd7fd23ef63d3ac494464a52e
macin-dev <ernesto.dev24@gmail.com>
2026-09-23 08:17:20 -0600

    Implement multi-user authentication

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, render_template
-from models import db, Device, CheckLog
+from flask import Flask, request, render_template, session, redirect
+from models import db, Device, CheckLog, User
 from checker import check_tcp
 from flask_migrate import Migrate
 from sqlalchemy import func
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "secret-key-netwatch"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///netwatch.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -22,7 +24,13 @@ with app.app_context():
 # Routes
 @app.route('/')
 def index():
-    # Retrieve data from the devices table
+    # Redirect unauthorized users
+    if not session.get("user_id"):
+        return redirect("/login")
+
+    # Find user
+    user = db.get_or_404(User, session.get("user_id"))
+
     # Find the highest log by ID for each device
     latest_device_log = (
         db.select(
@@ -38,6 +46,7 @@ def index():
         db.select(Device, CheckLog)
         .outerjoin(latest_device_log, Device.id == latest_device_log.c.device_id)
         .outerjoin(CheckLog, CheckLog.id == latest_device_log.c.max_id)
+        .where(Device.user_id == session.get("user_id"))
     )
 
     devices = db.session.execute(stmt).all()
@@ -56,7 +65,7 @@ def index():
         })
 
     # Return with a status code of success
-    return render_template('index.html', data=data), 200
+    return render_template('index.html', data=data, user=user), 200
 
 @app.route('/api/devices/<int:device_id>/check')
 def check_device(device_id):
@@ -96,7 +105,12 @@ def check_device(device_id):
 
 @app.route('/api/devices', methods=["GET", "POST"])
 def add_device():
-    user_id = 1
+
+    user_id = session.get("user_id")
+
+    # Deny access to not logged in users
+    if not user_id:
+        return {"error": "Unauthorized. Please log in."}, 401
 
     # [POST] requests
     # Handling incoming JSON
@@ -219,6 +233,98 @@ def device_logs(device_id):
         return render_template('partials/_history.html', device=device, logs=results), 200
 
     return results, 200
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # [POST]
+    if request.method == "POST":
+        email = request.form.get("email", "").strip()
+        password = request.form.get("password", "").strip()
+
+        errors = {}
+
+        # Validate data
+        if not email:
+            errors["email"] = "Missing email"
+        if not password:
+            errors['password'] = "Missing password"
+
+        if errors:
+            return errors, 400
+
+        # Check against database information
+        stmt = db.select(User).where(User.email == email)
+        user = db.session.scalar(stmt)
+
+        # Ensure user exists and password is correct
+        if not user or not check_password_hash(user.password_hash, password):
+            return {"message": "Invalid username and/or password"}, 401
+
+        # Remember which user has logged in
+        session["user_id"] = user.id
+
+        # Redirect
+        return redirect('/')
+
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    # [POST]
+    if request.method == "POST":
+        fullname = request.form.get("fullName", "").strip()
+        email = request.form.get("email", "").strip()
+        password = request.form.get("password", "").strip()
+        confirmation = request.form.get("confirmation", "").strip()
+
+        # Validate data
+        errors = {}
+
+        if not fullname:
+            errors["fullname"] = "Missing name"
+        if not email:
+            errors["email"] = "Missing email"
+        if not password:
+            errors["password"] = "Missing password"
+
+        if errors:
+            return errors, 400
+
+        # Check duplicate emails
+        existing_user = db.session.scalar(db.select(User).where(User.email == email))
+        if existing_user: 
+            return {"error": "Email is already registered"}, 400
+
+        if password != confirmation:
+            return {"message": "Password must be the same"}, 400
+        
+        # Hash the password
+        hashed = generate_password_hash(password)
+
+        # Create the user
+        new_user = User(
+            name=fullname,
+            email=email,
+            password_hash=hashed
+        )
+
+        # Commit it to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect("/login")
+
+    # [GET]
+    return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
 
 # Activate auto-reloader and debug mode
 if __name__ == "__main__": 
